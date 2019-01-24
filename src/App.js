a3dbb888f7235f4686c77632f0de7526a9c236d0
ongjp <ong.jp08@gmail.com>
2019-01-24 21:50:40 +0800

    last lesson state

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+      name: "Justin",
+      age: Math.floor(Math.random()*30)
+      },
+      {
+        name: "Paul",
+        age: Math.floor(Math.random()*30)
+      },
+      {
+        name: "Ong",
+        age: Math.floor(Math.random()*30)
+      },
+    ]
+  }
+
+  switchHandler = () => {
+    console.log("was Click!");
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Hi, I'm a React App</h1>
+        <button onClick={this.switchHandler}>Switch it up</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <p>This is really working!</p>
       </div>
-    );
+      );
   }
 }
 
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const person = (props) => {
+    return(
+        <p>My name is {props.name} and my age is {props.age}</p>
+    );
+}
+
+export default person;
